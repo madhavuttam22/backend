6a278ce1256391a785a1b0092bcef02f8782abee
Sandeep6268 <sandeepsinghkhicchi@example.com>
2025-07-02 22:07:17 +0530

    commit

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ class FirebaseUser(models.Model):
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20, blank=True)
     address = models.TextField(blank=True)
-    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+    # avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
 
     def __str__(self):
         return self.email
