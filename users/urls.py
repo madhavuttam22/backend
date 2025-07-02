87c5a17fadb8240a4e65bbd45015bbffcb95e997
Sandeep6268 <sandeepsinghkhicchi@example.com>
2025-07-03 01:11:30 +0530

    commit

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from .views import *
 urlpatterns = [
     # path('register/', register_firebase_user),
     path('profile/update/', FirebaseProfileUpdateView.as_view(), name='profile-update'),
+    path("profile/me/", FirebaseProfileMeView.as_view(), name="firebase-profile-me"),
 ]
