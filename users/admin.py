32463ef9928b252affa9fd245cd312eb8b4373f9
Sandeep6268 <sandeepsinghkhicchi@example.com>
2025-07-02 23:20:04 +0530

    commit

--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,7 @@ from .models import FirebaseUser
 class FirebaseUserAdmin(admin.ModelAdmin):
     list_display = ('email', 'full_name', 'phone', 'uid')
     search_fields = ('email', 'first_name', 'last_name', 'uid')
-    readonly_fields = ('uid')
+    # readonly_fields = ('uid')
 
     def full_name(self, obj):
         return f"{obj.first_name} {obj.last_name}".strip()
