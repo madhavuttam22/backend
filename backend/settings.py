521809ac195eaa6750f99d501fe63d0cfef05200
Sandeep6268 <sandeepsinghkhicchi@example.com>
2025-07-02 12:55:01 +0530

    Clean push without secrets

--- backend/settings.py
+++ backend/settings.py
@@ -98,11 +98,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default="postgresql://ecco_db_user:JeghPtxSgmpSSw0TvsPHYC5h8pZyINkO@dpg-d1ide3bipnbc73bifgb0-a.oregon-postgres.render.com/ecco_db",
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
