3e1f5bd6a404377538c98ae3c33a1c639ecb1397
Sandeep6268 <sandeepsinghkhicchi@example.com>
2025-07-02 21:04:57 +0530

    commit

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     # path('register/', register_firebase_user),
-    path('profile/', FirebaseUserUpdateView.as_view(), name='profile-update'),
+    path('profile/update/', FirebaseUserUpdateView.as_view(), name='profile-update'),
 ]
