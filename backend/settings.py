82e8c77622d71eef4afcbcc168ff2a1837d4be36
sandeep6268 <sandeepsinghkhicchi@gmail.com>
2025-09-10 23:18:25 +0530

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -46,11 +46,11 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'backend.middleware.firebase_auth.FirebaseAuthenticationMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
